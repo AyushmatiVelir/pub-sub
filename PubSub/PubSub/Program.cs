66fa3aa7b242aeb820a16fffac1514ba3cc5967e
AyushmatiVelir <ayushmati.das@velir.com>
2018-07-05 08:09:51 -0400

    Add File Reader

--- PubSub/PubSub/Program.cs
+++ PubSub/PubSub/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PubSub.Plugins;
+using PubSub.Processors;
 using PubSub.Publishers;
 using PubSub.Subscribers;
 
@@ -12,6 +14,23 @@ namespace PubSub
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length < 3)
+			{
+				System.Console.WriteLine("Please invoke with all the 3 arguments.");
+				return;
+			}
+			bool hasHeaders = false;
+			var settings = new TextFileSettings
+			{
+
+				ColumnHeadersInFirstLine = bool.TryParse(args[0], out hasHeaders) && hasHeaders,
+				ColumnSeparator = args[1],
+				Path = args[2]
+			};
+
+			ReadTextFileProcessor textFileProcessor=new ReadTextFileProcessor();
+			textFileProcessor.ReadData(settings);
+
 			var publisher = new Publisher();
 
 			var strategySubscriber = new StrategySubscriber();
